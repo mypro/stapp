d04d553e2dd65bb1a4b9c6255cb83bcc7fd51e6e
chenweichao <chenweichao@news.cn>
2014-08-20 16:56:02 +0800

    add  package declaration

--- src/test/java/com/test/Demo.java
+++ src/test/java/com/test/Demo.java
@@ -1,5 +1,7 @@
-public class Demo{
-	public static void main(String[] args){
+package com.test;
+
+public class Demo {
+	public static void main(String[] args) {
 		System.out.println("This is my test program.");
 		int a = 10;
 		int b = 20;
